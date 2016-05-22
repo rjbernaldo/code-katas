214e8f007adb4cf40704b001fb57b9812bb50878
Rj Bernaldo <rj@promisepay.com>
2016-05-23 07:34:07 +0800

    Add today's kata. Not entirely happy with my solution. Will need to refactor when I find time

---
+++ directions-reduction.js
@@ -0,0 +1,31 @@
+// http://www.codewars.com/kata/directions-reduction/javascript
+
+function dirReduc(arr){
+  var satisfied = false;
+  
+  while (!satisfied) {
+    var clean = true;
+    
+    for (var i = 0; i < arr.length - 1; i++) {
+      var current = arr[i];
+      var next = arr[i+1];
+      
+      var isOpposite = 
+        current === "NORTH" && next === "SOUTH" || 
+        current === "SOUTH" && next === "NORTH" ||
+        current === "EAST" && next === "WEST" || 
+        current === "WEST" && next === "EAST";
+        
+      if (isOpposite) {
+        arr.splice(i, 2);
+        clean = false;
+      }
+    }
+    
+    if (clean) {
+      satisfied = true;
+    }
+  }
+  
+  return arr;
+}
